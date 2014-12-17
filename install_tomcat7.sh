444635214c909478327e5de8d25bf42535adb3f0
Vadym Kovalenko <vadym.kovalenko@gmail.com>
2014-12-17 23:10:48 +0100

    minor fix

---
+++ install_tomcat7.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget -c -q "http://apache.volia.net/tomcat/tomcat-7/v7.0.57/bin/apache-tomcat-7.0.57.tar.gz" -O /opt/apache-tomcat-7.0.57.tar.gz
+wget -c -q "http://apache.volia.net/tomcat/tomcat-7/v7.0.57/src/apache-tomcat-7.0.57-src.tar.gz" -O /opt/apache-tomcat-7.0.57-src.tar.gz
+wget -c -q "http://apache.volia.net/ant/binaries/apache-ant-1.9.4-bin.tar.gz" -O /opt/apache-ant-1.9.4-bin.tar.gz 
+ln -s /opt/apache-ant-1.9.4 /opt/ant 
+echo ANT_HOME=/opt/ant >> /etc/environment
+ln -s /opt/ant/bin/ant /usr/bin/ant 
