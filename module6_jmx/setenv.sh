f9c6423245e3a6c3d6f629e4493205cbd47183bd
Vadym Kovalenko <vadym.kovalenko@gmail.com>
2014-12-23 20:35:43 +0100

    .

---
+++ module6_jmx/setenv.sh
@@ -0,0 +1,2 @@
+export JAVA_OPTS="$JAVA_OPTS -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9999 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
+
