f3ddbd83999b91f92fd694e5a5947b8188524e2b
Vadym Kovalenko <vadym.kovalenko@gmail.com>
2014-12-23 20:59:04 +0100

    .

--- module6_jmx/extra_setenv.sh
+++ module6_jmx/extra_setenv.sh
@@ -1,4 +1,10 @@
 -Dcom.sun.management.jmxremote.password.file=$CATALINA_BASE/conf/jmxremote.password
 -Dcom.sun.management.jmxremote.access.file=$CATALINA_BASE/conf/jmxremote.access
 -Dcom.sun.management.jmxremote.ssl=false
+-Xloggc:$CATALINA_HOME/logs/gc.log
+-XX:+PrintHeapAtGC
+-XX:+PrintGCDetails
+-XX:+PrintGCTimeStamps
+-XX:-HeapDumpOnOutOfMemoryError
+
 
