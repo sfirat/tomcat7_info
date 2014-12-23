4cbead9e433dc635878b6307fb0156280931b3d1
Vadym Kovalenko <vadym.kovalenko@gmail.com>
2014-12-23 20:47:35 +0100

    .

---
+++ module6_jmx/extra_setenv.sh
@@ -0,0 +1,4 @@
+-Dcom.sun.management.jmxremote.password.file=$CATALINA_BASE/conf/jmxremote.password
+-Dcom.sun.management.jmxremote.access.file=$CATALINA_BASE/conf/jmxremote.access
+-Dcom.sun.management.jmxremote.ssl=false
+
