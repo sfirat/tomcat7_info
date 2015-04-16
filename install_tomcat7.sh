08b4b7e8adf2caae78e51042cb78ed8da771d8b9
Vadym Kovalenko <vadym.kovalenko@gmail.com>
2015-04-16 07:52:36 +0200

    fix

--- install_tomcat7.sh
+++ install_tomcat7.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-wget -c -q "http://apache.volia.net/tomcat/tomcat-7/v7.0.57/bin/apache-tomcat-7.0.57.tar.gz" -O /opt/apache-tomcat-7.0.57.tar.gz
-#wget -c -q "http://apache.volia.net/tomcat/tomcat-7/v7.0.57/src/apache-tomcat-7.0.57-src.tar.gz" -O /opt/apache-tomcat-7.0.57-src.tar.gz
+wget -c -q "http://apache.volia.net/tomcat/tomcat-7/v7.0.61/bin/apache-tomcat-7.0.61.tar.gz" -O /opt/apache-tomcat-7.0.61.tar.gz
+#wget -c -q "http://apache.volia.net/tomcat/tomcat-7/v7.0.61/src/apache-tomcat-7.0.61-src.tar.gz" -O /opt/apache-tomcat-7.0.61-src.tar.gz
 #wget -c -q "http://apache.volia.net/ant/binaries/apache-ant-1.9.4-bin.tar.gz" -O /opt/apache-ant-1.9.4-bin.tar.gz 
 #ln -s /opt/apache-ant-1.9.4 /opt/ant 
 #echo ANT_HOME=/opt/ant >> /etc/environment
 #ln -s /opt/ant/bin/ant /usr/bin/ant 
-cd /opt && tar zxf apache-tomcat-7.0.57.tar.gz && mv apache-tomcat-7.0.57 tomcat7 && rm apache-tomcat-7.0.57.tar.gz
+cd /opt && tar zxf apache-tomcat-7.0.61.tar.gz && mv apache-tomcat-7.0.61 tomcat7 && rm apache-tomcat-7.0.61.tar.gz
