b84544476cf8368339b8d5a2e00d05bca9641b7e
Vadym Kovalenko <vadym.kovalenko@gmail.com>
2015-06-14 19:39:54 +0200

    v.62 update

--- install_jdk7.sh
+++ install_jdk7.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm -rf /usr/java/
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" \
 "http://download.oracle.com/otn-pub/java/jdk/7u71-b14/jdk-7u71-linux-x64.tar.gz" -O "/opt/jdk-7u71-linux-x64.tar.gz"
 cd /opt && tar zxf jdk-7u71-linux-x64.tar.gz && mkdir /usr/java/ && mv /opt/jdk1.7.0_71 /usr/java && rm /opt/jdk-7u71-linux-x64.tar.gz
