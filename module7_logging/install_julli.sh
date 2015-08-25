f9cf521ece9c4a7791fd1f7974bf3650c9398edf
root <root@vmi14656.contabo.net>
2015-08-25 20:31:28 +0200

    log4j fix

--- module7_logging/install_julli.sh
+++ module7_logging/install_julli.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-wget -c -q "http://apache.volia.net/tomcat/tomcat-7/v7.0.62/bin/extras/tomcat-juli-adapters.jar" -O /opt/tomcat-juli-adapters.jar
-wget -c -q "http://apache.volia.net/tomcat/tomcat-7/v7.0.62/bin/extras/tomcat-juli.jar" -O /opt/tomcat-juli.jar
-wget -c -q "http://apache.cp.if.ua/logging/log4j/1.2.17/log4j-1.2.17.tar.gz" -O /opt/log4j-1.2.17.tar.gz
-cd /opt && tar zxf log4j-1.2.17.tar.gz && cp apache-log4j-1.2.17/log4j-1.2.17.jar /opt/tomcat7/lib
+wget -c -q "http://apache.volia.net/tomcat/tomcat-7/v7.0.63/bin/extras/tomcat-juli-adapters.jar" -O /opt/tomcat-juli-adapters.jar
+wget -c -q "http://apache.volia.net/tomcat/tomcat-7/v7.0.63/bin/extras/tomcat-juli.jar" -O /opt/tomcat-juli.jar
+cp log4j-1.2.17.jar /opt/tomcat7/lib
 cp log4j.properties /opt/tomcat7/lib/log4j.properties
 mv /opt/tomcat-juli-adapters.jar /opt/tomcat7/lib/ 
 mv /opt/tomcat-juli.jar /opt/tomcat7/bin/
