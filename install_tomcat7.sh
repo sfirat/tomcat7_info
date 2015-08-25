cd2da68d1dec6ebb8a5681950d3305683b789c39
root <root@localhost.localdomain>
2015-08-25 04:45:25 -0400

    v63

--- install_tomcat7.sh
+++ install_tomcat7.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-wget -c -q "http://apache.volia.net/tomcat/tomcat-7/v7.0.62/bin/apache-tomcat-7.0.62.tar.gz" -O /opt/apache-tomcat-7.0.62.tar.gz
-#wget -c -q "http://apache.volia.net/tomcat/tomcat-7/v7.0.62/src/apache-tomcat-7.0.62-src.tar.gz" -O /opt/apache-tomcat-7.0.62-src.tar.gz
+wget -c -q "http://apache.volia.net/tomcat/tomcat-7/v7.0.63/bin/apache-tomcat-7.0.63.tar.gz" -O /opt/apache-tomcat-7.0.63.tar.gz
+#wget -c -q "http://apache.volia.net/tomcat/tomcat-7/v7.0.63/src/apache-tomcat-7.0.63-src.tar.gz" -O /opt/apache-tomcat-7.0.63-src.tar.gz
 #wget -c -q "http://apache.volia.net/ant/binaries/apache-ant-1.9.4-bin.tar.gz" -O /opt/apache-ant-1.9.4-bin.tar.gz 
 #ln -s /opt/apache-ant-1.9.4 /opt/ant 
 #echo ANT_HOME=/opt/ant >> /etc/environment
 #ln -s /opt/ant/bin/ant /usr/bin/ant 
-cd /opt && tar zxf apache-tomcat-7.0.62.tar.gz && mv apache-tomcat-7.0.62 tomcat7 && rm apache-tomcat-7.0.62.tar.gz
+cd /opt && tar zxf apache-tomcat-7.0.63.tar.gz && mv apache-tomcat-7.0.63 tomcat7 && rm apache-tomcat-7.0.63.tar.gz
