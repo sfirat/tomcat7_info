23468b5585cfa366a6ad453844e5f61e79ee2edd
Vadym Kovalenko <vadym.kovalenko@gmail.com>
2015-12-22 07:59:56 -0500

    v067

--- module7_logging/install_julli.sh
+++ module7_logging/install_julli.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-wget -c -q "http://apache.volia.net/tomcat/tomcat-7/v7.0.63/bin/extras/tomcat-juli-adapters.jar" -O /opt/tomcat-juli-adapters.jar
-wget -c -q "http://apache.volia.net/tomcat/tomcat-7/v7.0.63/bin/extras/tomcat-juli.jar" -O /opt/tomcat-juli.jar
+wget -c -q "http://apache.volia.net/tomcat/tomcat-7/v7.0.67/bin/extras/tomcat-juli-adapters.jar" -O /opt/tomcat-juli-adapters.jar
+wget -c -q "http://apache.volia.net/tomcat/tomcat-7/v7.0.67/bin/extras/tomcat-juli.jar" -O /opt/tomcat-juli.jar
 cp log4j-1.2.17.jar /opt/tomcat7/lib
 cp log4j.properties /opt/tomcat7/lib/log4j.properties
 mv /opt/tomcat-juli-adapters.jar /opt/tomcat7/lib/ 
