7150b1353a200b81999144698293733c4351a979
Vadym Kovalenko <vadym.kovalenko@gmail.com>
2015-12-22 17:36:04 -0500

    enable APR proto connector

---
+++ module10_perf/install_apr.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+yum install -y apr-devel.x86_64 make gcc apr-devel.x86_64
+cd /opt/tomcat7/bin/
+tar xf tomcat-native.tar.gz
+cd tomcat-native-1.1.33-src/jni/native
+./configure --with-apr=/usr/bin/apr-1-config --with-java-home=/usr/java/jdk1.7.0_71 && make && make install
+echo 'export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/apr/lib"' >> /opt/tomcat7/bin/setenv.sh
+cd /opt/tomcat/bin && rm -rf tomcat-native-1.1.33-src
