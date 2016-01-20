b314e9cf0bca55babbba3954a40c5ce57642c1e0
Vadym Kovalenko <vadym.kovalenko@gmail.com>
2016-01-20 08:12:46 +0000

    install_apr.sh edited online with Bitbucket

--- module10_perf/install_apr.sh
+++ module10_perf/install_apr.sh
@@ -5,4 +5,4 @@ tar xf tomcat-native.tar.gz
 cd tomcat-native-1.1.33-src/jni/native
 ./configure --with-apr=/usr/bin/apr-1-config --with-java-home=/usr/java/jdk1.7.0_71 --with-ssl && make && make install
 echo 'export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/apr/lib"' >> /opt/tomcat7/bin/setenv.sh
-cd /opt/tomcat/bin && rm -rf tomcat-native-1.1.33-src
+cd /opt/tomcat7/bin && rm -rf tomcat-native-1.1.33-src
