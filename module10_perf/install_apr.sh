ae6b8bd79122dab68043de3c8d36661464ed2b0f
Vadym Kovalenko <v.kovalenko@triangu.com>
2018-07-03 20:38:16 +0000

    install_apr.sh edited online with Bitbucket

--- module10_perf/install_apr.sh
+++ module10_perf/install_apr.sh
@@ -2,7 +2,7 @@
 yum install -y apr-devel.x86_64 make gcc apr-devel.x86_64 openssl.x86_64 openssl-devel.x86_64
 cd /opt/tomcat7/bin/
 tar xf tomcat-native.tar.gz
-cd tomcat-native-1.1.33-src/jni/native
+cd tomcat-native-1.2.16-src/jni/native
 ./configure --with-apr=/usr/bin/apr-1-config --with-java-home=/usr/java/jdk1.8.0_172 --with-ssl && make && make install
 echo 'export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/apr/lib"' >> /opt/tomcat7/bin/setenv.sh
-cd /opt/tomcat7/bin && rm -rf tomcat-native-1.1.33-src
+cd /opt/tomcat7/bin && rm -rf tomcat-native-1.2.16-src
