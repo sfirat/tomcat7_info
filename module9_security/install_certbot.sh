390e39a54a4db4e3b1ee9822c4f8dbfe5a0b59d4
Vadym Kovalenko <v.kovalenko@triangu.com>
2018-07-04 08:02:07 +0000

    install_certbot.sh edited online with Bitbucket

--- module9_security/install_certbot.sh
+++ module9_security/install_certbot.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+yum-config-manager --enable rhui-REGION-rhel-server-extras rhui-REGION-rhel-server-optional
 wget -r --no-parent -A 'epel-release-*.rpm' http://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/
 rpm -Uvh dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-*.rpm
 yum-config-manager --enable epel*
